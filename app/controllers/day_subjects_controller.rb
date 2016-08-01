5f4e96ef401eb6583583b0ca425fcfe4f7c05714
“Oleg <olegz@jetruby.com>
2016-08-01 10:40:19 +0300

    [#127414939] Adds the functional test for the DaySubjectController.

--- app/controllers/day_subjects_controller.rb
+++ app/controllers/day_subjects_controller.rb
@@ -35,6 +35,8 @@ class DaySubjectsController < ApplicationController
     end
   end
 
+  private
+
   def day_subject_params
     params.require(:day_subject).permit(:user_id, :title, :description)
   end
--- app/models/day_subject.rb
+++ app/models/day_subject.rb
@@ -2,6 +2,8 @@ class DaySubject < ActiveRecord::Base
   belongs_to :user
   has_one :image, as: :object
 
+  validates :title, length: { maximum: 40 }
+
   def owner?(user)
     self.user_id == user.id
   end
