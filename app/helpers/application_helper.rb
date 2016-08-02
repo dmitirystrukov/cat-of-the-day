061e67bb60524efeae0e976104c3eee58b0a7393
Dmitriy Strukov <dmitriy.strukov@jetruby.com>
2016-08-03 00:03:54 +0300

    [#127594313] Add auth links with refactoring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def sign_in?
+    current_user.present?
+  end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,5 +15,5 @@ Devise.setup do |config|
   config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
 
   config.reset_password_within = 6.hours
-  config.sign_out_via = :delete
+  config.sign_out_via = :get
 end
