a13e70409e4a44c6d56d8130e4ab12a262f9a70d
Dmitriy Strukov <dmitriy.strukov@jetruby.com>
2016-08-17 09:18:49 +0300

    Twitter api

--- config/initializers/000_omniauth.rb
+++ config/initializers/000_omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, OmniauthConfig['facebook']['token'], OmniauthConfig['facebook']['key']
+  provider :facebook,  OmniauthConfig['facebook']['token'],  OmniauthConfig['facebook']['key']
+  provider :twitter,   OmniauthConfig['twitter']['token'],   OmniauthConfig['twitter']['key']
   provider :vkontakte, OmniauthConfig['vkontakte']['token'], OmniauthConfig['vkontakte']['key']
 end
--- db/migrate/20160730103458_create_social_profiles.rb
+++ db/migrate/20160730103458_create_social_profiles.rb
@@ -3,6 +3,10 @@ class CreateSocialProfiles < ActiveRecord::Migration
     create_table :social_profiles do |t|
       t.integer :user_id
       t.string  :uid
+      t.string  :consumer_key
+      t.string  :consumer_secret
+      t.string  :access_token
+      t.string  :access_token_secret
       t.string  :service_name
 
       t.timestamps null: false
