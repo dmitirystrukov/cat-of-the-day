f79a1e0c7953f5219bc1bb6f946865f06d991fa0
Dmitriy Strukov <dmitriy.strukov@jetruby.com>
2017-03-19 14:07:12 +0300

    Added worker for inactivate old and irrelevant posts

--- app/services/providers/twitter.rb
+++ app/services/providers/twitter.rb
@@ -13,5 +13,13 @@ module Providers
     def update_with_image(social_params, user)
       @client.update_with_media(social_params[:message], image_file(social_params[:day_subject_image_id]))
     end
+
+    def post_exists?(id, options = {})
+      @client.status(id, options)
+      true
+
+      rescue ::Twitter::Error::NotFound
+        false
+    end
   end
 end
