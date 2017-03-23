17d5952e64936f05c412e951d17cd40f493d4b8f
Dmitriy Strukov <dmitiry_strukov2011@mail.ru>
2017-03-23 21:51:29 +0300

    Rubocop improvements v2

--- spec/models/day_subject_spec.rb
+++ spec/models/day_subject_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 RSpec.describe DaySubject, type: :model do
   describe '#connected_provider_names' do
-    let(:day_subject)  { create :day_subject_with_image }
+    let(:day_subject) { create :day_subject_with_image }
 
     let(:user) { create :user }
-    let!(:social_post) { create :social_post, user: user, day_subject_id: day_subject.to_param,
-                                          day_subject_image_id: day_subject.day_subject_images.first.to_param, service_name: 'TwitterPost' }
-
+    let!(:social_post) do
+      create :social_post, user: user, day_subject_id: day_subject.to_param,
+                           day_subject_image_id: day_subject.day_subject_images.first.to_param, service_name: 'TwitterPost'
+    end
 
     it 'should return array of providers' do
       expect(day_subject.connected_provider_names).to eq ['twitter']
