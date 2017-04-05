5da4a02cca1c8177e9097cc59ddd32970dad696e
Dmitriy Strukov <dmitriy.strukov@jetruby.com>
2017-04-05 22:50:55 +0300

    [#143152153] Added fields rendering

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  STATISTIC_FIELD_ICONS = {
+    'likes_count'    => 'fa fa-heart',
+    'reposts_count'  => 'fa fa-retweet',
+    'comments_count' => 'fa fa-comments-o'
+  }
+
   def provider_active_class(provider_name)
     provider_active?(provider_name) ? 'active' : ''
   end
@@ -7,7 +13,7 @@ module ApplicationHelper
     current_user.social_profiles.any? { |profile| profile.service_name == provider_name }
   end
 
-  def tab_active_class(record, collection=SocialProvider.providers_list)
+  def tab_active_class(record, collection = SocialProvider.providers_list)
     record_first?(record, collection) ? 'active' : ''
   end
 
@@ -17,5 +23,17 @@ module ApplicationHelper
 
   def profile_owner?(user)
     current_user == user
+  end
+
+  def post_statistic_fields(data)
+    data.instance_values.map do |key, value|
+      post_field(value, STATISTIC_FIELD_ICONS[key])
+    end
+  end
+
+  def post_field(value, icon_class)
+    content_tag :i, class: icon_class do
+      concat content_tag :span, "#{value}".html_safe
+    end
   end
 end
