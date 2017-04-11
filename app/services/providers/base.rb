365e0c5562bac6def3ff3208122de2dc9c3643bf
Dmitriy Strukov <dmitiry_strukov2011@mail.ru>
2017-04-11 20:48:58 +0300

    Improved store dir method

--- app/services/providers/base.rb
+++ app/services/providers/base.rb
@@ -5,7 +5,14 @@ module Providers
     private
 
     def image_file(image_id)
-      File.new(DaySubjectImage.find(image_id).url.path)
+      day_subject_image = DaySubjectImage.find(image_id)
+      image_full_path = helpers.asset_url(day_subject_image.url)
+
+      File.new open(image_full_path)
+    end
+
+    def helpers
+      ActionController::Base.helpers
     end
   end
 end
